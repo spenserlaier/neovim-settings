503d55afc0d3cb7f13eec3524d05ae486eb4233d
Spenser Laier <spenserlaier@gmail.com>
2025-07-18 13:18:59 -0400

    add tsserver/ts_ls

--- init.lua
+++ init.lua
@@ -682,6 +682,10 @@ require('lazy').setup({
         pyright = {
           settings = {},
         },
+        ts_ls = {
+          filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+          settings = {},
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
