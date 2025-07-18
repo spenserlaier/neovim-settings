d04a8bb6feb5f2c094b96f9f7fcbe92ccca211ce
Spenser Laier <spenserlaier@gmail.com>
2025-07-18 12:32:32 -0400

    modify default tab behavior

--- init.lua
+++ init.lua
@@ -90,8 +90,15 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set Default Tab Behavior
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
