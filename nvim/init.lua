fb47aa6a1b40d3506fe0ceac8376b6105185c48c
spenser <spenserlaier@gmail.com>
2025-10-01 19:18:39 -0400

    change colorscheme to tokyonight-night

--- nvim/init.lua
+++ nvim/init.lua
@@ -917,7 +917,9 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-
+  {
+    'folke/tokyonight.nvim',
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -928,7 +930,7 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('dracula').setup {
+      require('tokyonight').setup {
         styles = {
           comments = { italic = true }, -- Disable italics in comments
         },
@@ -937,7 +939,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'dracula'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
@@ -1016,6 +1018,7 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
+
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
