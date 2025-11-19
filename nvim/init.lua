516e467d22b5be60c0f0acd8db0fdf203ed37aa7
spenser <spenserlaier@gmail.com>
2025-11-19 18:40:12 -0500

    ensure consistent bracket matching with large code blocks

--- nvim/init.lua
+++ nvim/init.lua
@@ -264,9 +264,13 @@ require('lazy').setup({
   {
     'andymass/vim-matchup',
     event = 'BufReadPost', -- Load it lazily when you open a file
-    config = function()
+    init = function()
       -- Optional: Enables a popup showing the "start" of the block when you jump to the end
       -- (Useful in Python so you know what function you are closing)
+      vim.g.matchup_matchparen_stopline = 40000
+      vim.g.matchup_matchparen_timeout = 5000
+      vim.g.matchup_motion_timeout = 5000
+      vim.g.matchup_matchparen_deferred = 1
       vim.g.matchup_matchparen_offscreen = { method = 'popup' }
     end,
   },
