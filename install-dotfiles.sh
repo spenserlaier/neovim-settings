4408345e3c49efae732d6f30762edc852eaff0f0
Spenser Laier <spenserlaier@gmail.com>
2026-02-15 16:27:10 -0500

    begin migrating to mise for auto installation

---
+++ install-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# Get the directory of this script, even if called from elsewhere
+DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Neovim
+mkdir -p ~/.config
+ln -sf "$DOTFILES/nvim" ~/.config/nvim
+
+# Kitty
+mkdir -p ~/.config/kitty
+ln -sf "$DOTFILES/kitty/kitty.conf" ~/.config/kitty/kitty.conf
+
+# Tmux
+ln -sf "$DOTFILES/tmux/tmux.conf" ~/.tmux.conf
+
+echo "Dotfiles installed!"
+
+# TODO: add code for fish config
+
