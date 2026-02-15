4ea9adf0eb2727009967aa610a771d327bd62131
Spenser Laier <spenserlaier@gmail.com>
2026-02-15 16:43:43 -0500

    update mise dependencies; update fish config

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+starship init fish | source
+set -g fish_key_bindings fish_vi_key_bindings
+atuin init fish --disable-up-arrow | source
+# use z instead of cd automatically
+zoxide init fish | source
+# make 'cd' behave like z
+alias cd="z"
+bind \t _atuin_search
+end
