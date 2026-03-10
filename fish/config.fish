e129955eb40d1a44b2ea58e3be5cd48cd5974634
Spenser Laier <spenserlaier@gmail.com>
2026-03-10 02:44:07 -0400

    add common fish paths to default config

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/share/mise/shims
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.npm-global/bin
+fish_add_path ~/.pyenv/bin
 mise activate fish | source
 starship init fish | source
 set -g fish_key_bindings fish_vi_key_bindings
