4bab599e29038733ee18987cc0fa0271df4afbb4
Spenser Laier <spenserlaier@gmail.com>
2026-03-10 03:24:17 -0400

    add terminal-first defaults to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx MANPAGER "nvim +Man!"
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/share/mise/shims
 fish_add_path ~/.cargo/bin
