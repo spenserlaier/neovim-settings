1515c14592d1f1ad030ca3d8994f0f7d90c32c76
spenser laier <spenserlaier@gmail.com>
2026-04-17 21:55:11 -0400

    replace surround keybindings
    resolves conflict with keybindings in flash.nvim, allowing
    both plugins to coexist

--- nvim/init.lua
+++ nvim/init.lua
@@ -1085,6 +1085,27 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- Add/delete/replace surroundings (brackets, quotes, etc.)
+  --
+  -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+  -- - sd'   - [S]urround [D]elete [']quotes
+  -- - sr)'  - [S]urround [R]eplace [)] [']
+  {
+    'https://github.com/kylechui/nvim-surround',
+    opts = {
+      mappings = {
+        add = 'ys',
+        delete = 'ds',
+        replace = 'cs',
+        -- These are less common, but mapped to keep them out of Flash's way
+        find = 'yf',
+        find_left = 'yF',
+        highlight = 'yh',
+        update_n_lines = 'yn',
+      },
+    },
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
@@ -1095,13 +1116,6 @@ require('lazy').setup({
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
